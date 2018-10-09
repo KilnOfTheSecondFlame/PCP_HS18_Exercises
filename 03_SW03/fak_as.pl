fbbee0eadfc9e8e705260e5e555373ac099df2b2
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-10-09 07:55:12 +0200

    Finished Exercise 2

--- 03_SW03/fak_as.pl
+++ 03_SW03/fak_as.pl
@@ -6,9 +6,11 @@ fak_tr(N, Mult, F) :-
 	Mult1 is N * Mult,
 	fak_tr(N1, Mult1, F).
 
-fak(N, F) :- fak_as(N, F), ! , write("Hinweis: Fakultaet "), write(N), write(" war gespeichert").
+fak(N, F) :- fak_s(N,F), !, write("Hinweis: Fakultaet "), write(N), write(" war gespeichert").
+fak(N, F) :- fak_as(N, F), !.
 
 :- dynamic fak_as/2.
+:- dynamic fak_s/2.
 
 fak_as(0, 1).
 fak_as(N, F):-
@@ -16,4 +18,7 @@ fak_as(N, F):-
 	N1 is N-1,
 	fak_as(N1, F1),
 	F is N*F1,
-	asserta(fak_as(N, F)).
+	asserta(fak_as(N, F)),
+	asserta(fak_s(N, F)).
+
+fak_clear() :- retractall(fak_s(_,_)), write("Hinweis: Alle gespeicherten Werte wurden geloescht.").
