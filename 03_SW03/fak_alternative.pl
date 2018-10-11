7a44f6ba9e543d149e7485b78beca0f4d41594cf
Thushjandan Ponnudurai <thushjandan@ponnudurai.ch>
2018-10-11 13:37:21 +0200

    alternative version of Task 2 and a bugfix in task 6

---
+++ 03_SW03/fak_alternative.pl
@@ -0,0 +1,14 @@
+:- dynamic fak_as/2.
+fak(N, F) :- fak_as(N,F), !,write("Hinweis: Fakultaet "), write(N), write(" war gespeichert").
+
+%:- dynamic fak_s/2.
+
+fak(0, 1).
+fak(N, F):-
+	N > 0,
+	N1 is N-1,
+	fak(N1, F1),
+	F is N*F1,
+	asserta(fak_as(N, F)).
+
+fak_clear() :- retractall(fak_as(_,_)), write("Hinweis: Alle gespeicherten Werte wurden geloescht.").
--- 03_SW03/red_cut.pl
+++ 03_SW03/red_cut.pl
@@ -1,7 +1,8 @@
 mem(X, [X | _]).
 mem(X, [_| Tail]) :- mem(X, Tail).
 
+redCut(X, Y, Z, L) :- L = [_, _, _], mem(a, L),  mem(b, L), mem(c, L), !.
 % Only one permutation
-% L = [_, _, _], mem(a, L), !,  mem(b, L), mem(c, L).
+% L = [_, _, _], mem(a, L),  mem(b, L), mem(c, L), !.
 % Es ist ein Red-Cut, da die deklarative Bedeutung des Praedikat veraendert wird
 % D.h. weitere moegliche Loesungen werden abgeschnitten
