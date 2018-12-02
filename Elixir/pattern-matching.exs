77606fbc058ff73520165043f749f1620426af78
Thushjandan Ponnudurai <thushjandan@ponnudurai.ch>
2018-12-02 17:09:41 +0100

    pattern matching example code

--- Elixir/pattern-matching.exs
+++ Elixir/pattern-matching.exs
@@ -1,10 +1,16 @@
+a = 3
+3 = a
+# 2 = a
+# ** (MatchError) no match of right hand side value: 3<
+IO.puts(a)
+
 greeting = "hello"
 greet = fn
-  # Matching auf hello
+  # Matching auf hello. greeting nicht rebinden
   (^greeting, name) -> "Hi #{name}"
   # Matching auf Symbol bye
   (:bye, name)      -> "Bye #{name}!"
-  # Falls sonst nichts matcht, dann dieser Fall
+  # Falls sonst nichts matcht, dann dieser Fall. greeting wird rebindet
   (greeting, name)  -> "Who are you? #{greeting} #{name}"
 end
 
