dd2b8ba544a49d1d8e4e4b9aaff5152df03b8c91
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-12-10 17:17:31 +0100

    Add print to Stream

--- Elixir/enums-streams.exs
+++ Elixir/enums-streams.exs
@@ -1,8 +1,11 @@
 defmodule PCPStreams do
-  def PowerSumPositive(list) do
+  def powerSumPositive(list) when is_list(list) do
     list
     |> Enum.filter(&(&1 > 0))
-    |> Enum.map(&1 * &1)
+    |> Enum.map(&(&1 * &1))
     |> Enum.reduce(0, &(&1 + &2))
+    |> IO.puts()
   end
 end
+
+PCPStreams.powerSumPositive([-1,2,4,6])
