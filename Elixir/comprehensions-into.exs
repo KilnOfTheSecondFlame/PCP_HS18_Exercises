32388a7d5084424fabe086931982264f5a0036ea
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-12-11 17:17:13 +0100

    Add more collections and flavourtext

--- Elixir/comprehensions-into.exs
+++ Elixir/comprehensions-into.exs
@@ -1,9 +1,13 @@
+IO.puts("--- Lists ---\n")
 # Kennen wir von pipes schon
 list = [1, 2, 3, 4, 5]
-for x <- list, do: x*x
+[head | _tail] = list
+IO.puts(head)
+for x <- list, do: IO.puts(x*x)
 
 # Funktioniert jedoch auch mit anderen Collections
 
+IO.puts("\n\n--- Keyword Lists ---\n")
 # Keyword list
 dep_bundesrat = [efd: "Maurer", eda: "Cassis", edi: "Berset",
   wbf: "Schneider-Ammann", vbs: "Parmelin", ejpd: "Sommaruga", uvek: "Leuthard"]
@@ -12,3 +16,30 @@ IO.puts(dep_bundesrat[:efd])
 for  {dep, br} <- dep_bundesrat do
   IO.puts(String.upcase("#{dep}") <> ": " <> br)
 end
+
+IO.puts("\n\n--- Maps ---\n")
+# Maps
+alphabet = %{
+  1 => "a",
+  3 => "c",
+  2 => "b"
+}
+IO.puts(alphabet[2])
+resultat =
+for {index, letter} <- alphabet, do: {index, letter}
+IO.inspect(resultat)
+
+IO.puts("\n\n--- Binaries ---\n")
+# Binaries
+string = "PCP.HS2018"
+resultat =
+for <<c <- string>>, do: <<c>>
+IO.inspect(resultat)
+
+IO.puts("\n\n--- Nested Generators ---\n")
+# Mehrere Generatoren
+anzahl = [1, 2, 3]
+for n <- anzahl, wiederholungen <- 1..n do
+  IO.puts(String.duplicate("Na", wiederholungen))
+end
+IO.puts("Batman!")
