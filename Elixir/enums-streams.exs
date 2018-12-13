928a46c63cf2847e90b4c8d32523f814f2a578e8
Thushjandan Ponnudurai <thushjandan@ponnudurai.ch>
2018-12-13 10:50:38 +0100

    :sparkles: using stream functions

--- Elixir/enums-streams.exs
+++ Elixir/enums-streams.exs
@@ -15,6 +15,7 @@
 
 defmodule PCPStreams do
   @moduledoc false
+  # Enum functions sind eager
   def powerSumPositive(list) when is_list(list) do
     list
     |> Enum.filter(&(&1 > 0))
@@ -26,7 +27,23 @@ defmodule PCPStreams do
   def powerSumPositive(_) do
   	IO.puts("Attribute is not a list")
   end
+
+  # Stream sind lazy
+  def powerSumPositiveStream(list) when is_list(list) do
+    list
+    |> Stream.filter(&(&1 > 0))
+    |> Stream.map(&(&1 * &1))
+    |> Stream.take(10)
+    |> Enum.to_list()
+    |> IO.inspect()
+  end
+
+  def powerSumPositiveStream(_) do
+  	IO.puts("Attribute is not a list")
+  end
+
 end
 
 PCPStreams.powerSumPositive([-1,2,4,6])
 PCPStreams.powerSumPositive(%{name: "Fred", age: "56"})
+PCPStreams.powerSumPositiveStream(Enum.to_list(1..100_000))
